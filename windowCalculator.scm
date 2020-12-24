0cac7688e77cd9b1d74275ef62616889ded8c5c0
Alexander Mohn <pogrmman@gmail.com>
2020-12-24 17:05:46 -0600

    Calculate Heliocentric Longitude

--- windowCalculator.scm
+++ windowCalculator.scm
@@ -1,3 +1,15 @@
+;;; Observing Window Calculator
+;;; Calculates dates on which an object is high enough at a given time to be worth observing
+
+;; Imports
+(import ((scheme base)
+         (scheme cxr)
+         (scheme inexact)))
+
+;; Positition of Sun Calculations
+;; Algorithms from Reda, I., Andreas, A. (January, 2008). Solar Position Algorithm for
+;;                     Solar Radiation Applications. Golden, CO: National Renewable Energy Laboratory.
+; Julian Day Calculation
 (define (jul-day year month day)
   (let ((month (if (< 2 month)
                    month
@@ -12,15 +24,19 @@
          (+ 2 (- a) (truncate-quotient a 4))
          (- 1524.5)))))
 
+; Julian Ephemeris Day Calculation
 (define (jul-eph-day jul-day delta-t)
   (+ jul-day (/ delta-t 86400)))
 
+; Julian Century Calculation
 (define (jul-century jul-day)
   (/ (- jul-day 2451545) 36525))
 
+; Julian Ephemeris Millenium Calculation
 (define (jul-eph-millenium jul-eph-century)
   (/ jul-eph-century 10))
 
+; Reducer for Heliocentric Longitude, Latitude, and Radius
 (define (reduce-table tbl jul-eph-millenium)
   (let kernel ((accumulator 0)
                (row (car tbl))
@@ -35,6 +51,39 @@
                           (cos (+ (cadr row)
                                   (* jul-eph-millenium (caddr row)))))))))
 
+; Periodic Term Calculation
+(define (calculate-heliocentric-term term-list julian-eph-millenium)
+  (let kernel ((sum (car term-list))
+               (terms (cdr term-list))
+               (counter 1))
+    (if (not (null? terms))
+        (kernel (+ sum (* (car terms) (expt julian-eph-millenium counter)))
+                (cdr terms)
+                (+ counter 1))
+        (let* ((result (/ (* 180 (/ sum (expt 10 8))) 3.141592653589793238462))
+               (frac (- (/ result 360) (truncate-quotient result 360))))
+          (if (positive? result)
+              (* 360 frac)
+              (- 360 (* 360 frac)))))))
+
+; Heliocentric Longitude Calculation
+(define (calculate-heliocentric-longitude julian-eph-millenium)
+  (calculate-heliocentric-term (list (reduce-table l0-table julian-eph-millennium)
+                                     (reduce-table l1-table julian-eph-millenium)
+                                     (reduce-table l2-table julian-eph-millenium)
+                                     (reduce-table l3-table julian-eph-millenium)
+                                     (reduce-table l4-table julian-eph-millenium)
+                                     (reduce-table l5-table julian-eph-millenium))
+                               julian-eph-millenium))
+
+; Heliocentric Latitude Calculation
+
+
+; Heliocentric Radius Vector Calculation
+
+
+; Tables for Earth Periodic Terms
+; Heliocentric Longitude
 (define l0-table
   '((175347046 0 0)
     (341656 4.6692568 6283.07585)
@@ -100,3 +149,78 @@
     (30 0.44 83996.85)
     (30 2.74 1349.87)
     (25 3.16 4690.48)))
+
+(define l1-table
+  '((628331966747 0 0)
+    (206059 2.678235 6283.07585)
+    (4303 2.6351 12566.1517)
+    (425 1.59 3.523)
+    (119 5.796 26.298)
+    (109 2.966 1577.344)
+    (93 2.59 18849.23)
+    (72 1.14 529.69)
+    (68 1.87 398.15)
+    (67 4.41 5507.55)
+    (59 2.89 5223.69)
+    (56 2.17 155.42)
+    (45 0.4 796.3)
+    (36 0.47 775.52)
+    (29 2.65 7.11)
+    (21 5.34 0.98)
+    (19 1.85 5486.78)
+    (19 4.97 213.3)
+    (17 2.99 6275.96)
+    (16 0.03 2544.31)
+    (16 1.43 2146.17)
+    (15 1.21 10977.08)
+    (12 2.83 1748.02)
+    (12 3.26 5088.63)
+    (12 5.27 1194.45)
+    (12 2.08 4694)
+    (11 0.77 553.57)
+    (10 1.3 6286.6)
+    (10 4.24 1349.87)
+    (9 2.7 242.73)
+    (9 5.64 951.72)
+    (8 5.3 2352.87)
+    (6 2.65 9437.76)
+    (6 4.67 4690.48)))
+
+(define l2-table
+  '((52919 0 0)
+    (8720 1.0721 6283.0758)
+    (309 0.867 12566.152)
+    (27 0.05 3.52)
+    (16 5.19 26.3)
+    (16 3.68 155.42)
+    (10 0.76 18849.23)
+    (9 2.06 77713.77)
+    (7 0.83 775.52)
+    (5 4.66 1577.34)
+    (4 1.03 7.11)
+    (4 3.44 5573.14)
+    (3 5.14 796.3)
+    (3 6.05 5507.55)
+    (3 1.19 242.73)
+    (3 6.12 529.69)
+    (3 0.31 398.15)
+    (3 2.28 553.57)
+    (2 4.38 5223.69)
+    (2 3.75 0.98)))
+
+(define l3-table
+  '((289 5.844 6283.076)
+    (35 0 0)
+    (17 5.49 12566.15)
+    (3 5.2 155.42)
+    (1 4.72 3.52)
+    (1 5.3 18849.23)
+    (1 5.97 242.73)))
+
+(define l4-table
+  '((114 3.142 0)
+    (8 4.13 6283.08)
+    (1 3.84 12566.15)))
+
+(define l5-table
+  '((1 3.14 0)))
